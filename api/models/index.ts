7ff231df013b09b989fb9dbceb34e4ab50008323
dodosaido <bancadt@gmail.com>
2025-09-12 22:44:33 +0700

    add pagination

--- api/models/index.ts
+++ api/models/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import type { AnimeList, Anime, Pagelist } from "../../types";
+import type { AnimeList, Anime, Pagelist } from "../../types.js";
 
 /*
 ===================
