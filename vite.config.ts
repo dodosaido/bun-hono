f9abab44d2e77c12ff3fad7d35d31cff3b35975b
dodosaido <bancadt@gmail.com>
2025-09-12 22:52:48 +0700

    fix types.d

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     resolve: {
         alias: {
             "@components": path.resolve(__dirname, "src/components"),
-            "@types": path.resolve(__dirname, "types.ts"),
+            "@types": path.resolve(__dirname, "types.d.ts"),
         },
     },
 });
